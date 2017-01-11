73afc2a3c240c4765b5761981805d47cf41f08bb
Karim Ellaisy <karim@icomeupmarketing.com>
2017-01-11 11:30:26 -0500

    format and add polynomial regression formula

--- src/main.ino
+++ src/main.ino
@@ -1,3 +1,4 @@
+
 #include <Arduino.h>
 
 #include <Wire.h>
@@ -5,8 +6,8 @@
 
 I2CEncoder encoder;
 
-const int fPin = 3;  
-const long len = 233280;// pins
+const int fPin = 3;
+const long len = 233280; // pins
 const int sPin = 2;
 const int vexPin = A2;
 const int resetPin = 11;
@@ -14,30 +15,29 @@ const int camPin = 9;
 int in1Pin = 7;
 int in2Pin = 6;
 
-int count = 1;               // ticks of the rotary encoder
+int count = 1; // ticks of the rotary encoder
 int oldA = LOW;
 
-int pictures;                // number of pictures to take at each stop
-long dur;// duration between each stop
+int pictures; // number of pictures to take at each stop
+long dur;     // duration between each stop
 long secsBefore;
 long secsAfter;
 
-int repeats = 0;             // number of repeats that took place
-int event;                   // timer
+int repeats = 0; // number of repeats that took place
+int event;       // timer
 
 void setup() {
-    Wire.begin();
-   //Serial.begin(9600);
-   Serial.begin(9600);  //initializing bluetooth serial connection
-   pinMode(19, INPUT_PULLUP);
-   pinMode(18, INPUT_PULLUP);
-   pinMode(vexPin, OUTPUT);
-   pinMode(fPin, INPUT);
-   pinMode(sPin, INPUT);
-   pinMode(resetPin, INPUT);
-   pinMode(camPin, OUTPUT);
-   digitalWrite(resetPin, HIGH); 
-    encoder.init(MOTOR_393_SPEED_ROTATIONS, MOTOR_393_TIME_DELTA);
+  Wire.begin();
+  Serial.begin(9600); // initializing bluetooth serial connection
+  pinMode(19, INPUT_PULLUP);
+  pinMode(18, INPUT_PULLUP);
+  pinMode(vexPin, OUTPUT);
+  pinMode(fPin, INPUT);
+  pinMode(sPin, INPUT);
+  pinMode(resetPin, INPUT);
+  pinMode(camPin, OUTPUT);
+  digitalWrite(resetPin, HIGH);
+  encoder.init(MOTOR_393_SPEED_ROTATIONS, MOTOR_393_TIME_DELTA);
   getInput1();
   getInput2();
   getInput3();
@@ -45,35 +45,37 @@ void setup() {
 
 void testBt() {
   Serial.println("Test3");
-//  Serial2.println("Test2");
-//  Serial1.println("Test1");
+  //  Serial2.println("Test2");
+  //  Serial1.println("Test1");
 }
-
 
 void loop() {
   initslide();
 
-   int offset = floor (60 -((7776*(0.24339217+0.0020943471*pictures+0.0000048879012*(pictures*pictures)-.5))/(pictures-1)));
+  int offset =
+      floor(60 - ((7776 * (0.24339217 + 0.0020943471 * pictures +
+                           0.0000048879012 * (pictures * pictures) - .5)) /
+                  (pictures - 1)));
 
   Serial.println("in loop");
-    Serial.print("1 of ");
-    Serial.print(pictures);
+  Serial.print("1 of ");
+  Serial.print(pictures);
   takePicture();
   Serial.println(-dur);
-  for (int i = 0; i<pictures - 1; i++) {
-    while(encoder.getRawPosition() > -dur + offset ){
-      if(Serial.available()) {
+  for (int i = 0; i < pictures - 1; i++) {
+    while (encoder.getRawPosition() > -dur + offset) {
+      if (Serial.available()) {
         char input = Serial.read();
-        if(input == '0') {
+        if (input == '0') {
           setMotor(0, true);
           while (!Serial.available()) {
             ;
           }
           char input2 = Serial.read();
-          if(input2 == '1') {
+          if (input2 == '1') {
             continue;
           }
-        }        
+        }
         if (input == '2') {
           reset();
         }
@@ -83,11 +85,12 @@ void loop() {
       }
       setMotor(255, true);
     }
-    
-    setMotor(150, false); //moving motor backwards after each stop to counter the "drift", was 200, just changed to 150
-    delay(150); //the time the motor moves backwards to counter the "drift"
-    
-    Serial.print(i+2);
+
+    setMotor(150, false); // moving motor backwards after each stop to counter
+                          // the "drift", was 200, just changed to 150
+    delay(150); // the time the motor moves backwards to counter the "drift"
+
+    Serial.print(i + 2);
     Serial.print(" of ");
     Serial.print(pictures);
     takePicture();
@@ -97,47 +100,48 @@ void loop() {
   }
 }
 
-void setMotor(int speed, boolean reverse)
-{
+void setMotor(int speed, boolean reverse) {
   long var = encoder.getRawPosition();
   analogWrite(vexPin, speed);
-  digitalWrite(in1Pin, ! reverse);
+  digitalWrite(in1Pin, !reverse);
   digitalWrite(in2Pin, reverse);
   Serial.println(var);
 }
 
 void takePicture() {
-    setMotor(70, false);
-    delay((secsBefore * 1000));
-    digitalWrite(camPin, HIGH);
-    delay(300);
-    digitalWrite(camPin, LOW);
-    Serial.println("Took Picture!");
-    Serial.println(encoder.getRawPosition());
-    Serial.println(-dur);
-    delay((secsAfter * 1000));
-    repeats++;
-    encoder.zero();
+  setMotor(70, false);
+  delay((secsBefore * 1000));
+  digitalWrite(camPin, HIGH);
+  delay(300);
+  digitalWrite(camPin, LOW);
+  Serial.println("Took Picture!");
+  Serial.println(encoder.getRawPosition());
+  Serial.println(-dur);
+  delay((secsAfter * 1000));
+  repeats++;
+  encoder.zero();
 }
 
 void reset() {
   Serial.println("Resetting");
-  while(digitalRead(resetPin) != 0) {
+  while (digitalRead(resetPin) != 0) {
     setMotor(255, false);
   }
 }
 
 void initslide() {
   setMotor(255, true);
-  while(encoder.getRawPosition() > -500) {
-   setMotor(255, true);
+  while (encoder.getRawPosition() > -500) {
+    setMotor(255, true);
   }
-    
-  while(digitalRead(resetPin) != 0) {
+
+  while (digitalRead(resetPin) != 0) {
     setMotor(255, false);
   }
   encoder.zero();
-  while(encoder.getRawPosition() > -1500) { //changed from -2500 to 1500 to give more bar length in beginning...
+  while (encoder.getRawPosition() > -1500) { // changed from -2500 to 1500 to
+                                             // give more bar length in
+                                             // beginning...
     setMotor(255, true);
   }
   setMotor(150, false);
@@ -161,20 +165,21 @@ void getInput1() {
   if (input3 == '0') {
     if (input4 == '0') {
       pictures = input5 - '0';
-    }
-    else {
+    } else {
       sprintf(test, "%c%c", input4, input5);
       String pics = String(test);
       pictures = pics.toInt();
     }
-  }
-  else {
+  } else {
     sprintf(test, "%c%c%c", input3, input4, input5);
     String pics = String(test);
     pictures = pics.toInt();
   }
   Serial.println(pictures);
-  dur = (long) len / (pictures-1);  //changed this to pictures minus 1 because that should be the number of stops....OOPS!!!! this will change everything!
+  dur = (long)len / (pictures - 1); // changed this to pictures minus 1 because
+                                    // that should be the number of
+                                    // stops....OOPS!!!! this will change
+                                    // everything!
 }
 void getInput2() {
   char test[10];
@@ -193,17 +198,15 @@ void getInput2() {
   if (input3 == '0') {
     if (input4 == '0') {
       secsBefore = input5 - '0';
-    }
-    else {
+    } else {
       sprintf(test, "%c%c", input4, input5);
       String pics = String(test);
-      secsBefore = (pics.toInt()) * 1; //think 1000 is too much, trying 1
+      secsBefore = (pics.toInt()) * 1; // think 1000 is too much, trying 1
     }
-  }
-  else {
+  } else {
     sprintf(test, "%c%c%c", input3, input4, input5);
     String pics = String(test);
-    secsBefore = (pics.toInt()) * 1; //think 1000 isn't right, trying 1
+    secsBefore = (pics.toInt()) * 1; // think 1000 isn't right, trying 1
   }
   Serial.println(secsBefore);
 }
@@ -224,17 +227,23 @@ void getInput3() {
   if (input3 == '0') {
     if (input4 == '0') {
       secsAfter = input5 - '0';
-    }
-    else {
+    } else {
       sprintf(test, "%c%c", input4, input5);
       String pics = String(test);
-      secsAfter = (pics.toInt()) * 1; //changed from 1000 to 1
+      secsAfter = (pics.toInt()) * 1; // changed from 1000 to 1
     }
-  }
-  else {
+  } else {
     sprintf(test, "%c%c%c", input3, input4, input5);
     String pics = String(test);
-    secsAfter = (pics.toInt()) * 1; //changed from 1000 to 1, not needed as it is 3 digits
+    secsAfter = (pics.toInt()) *
+                1; // changed from 1000 to 1, not needed as it is 3 digits
   }
   Serial.println(secsAfter);
 }
+
+
+Format! Style:
+
+C++ online code formatter © 2014 by KrzaQ
+
+Powered by vibe.d, the D language and clang-format
